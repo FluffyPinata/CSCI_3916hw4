8f960ee21fc7e83a83af7da0b5fba1fe719c223b
jonesga1 <gabriel.jones@ucdenver.edu>
2019-04-07 23:54:35 -0600

    change doc to data

--- server.js
+++ server.js
@@ -199,10 +199,10 @@ router.route('/reviews')
         let token2 = token1.split(' ');
         let token3 = jwt.verify(token2[1], process.env.SECRET_KEY);
 
-        Movie.findOne( { title: req.body.movieTitle }, function(err, data) {
+        Movie.findOne( { title: req.body.movieTitle }, function(err, doc) {
             if (err) {
                 res.json({message: 'General error'});
-            } else if (data.length !== 0) {
+            } else if (doc !== null) {
                 var review = new Review();
                 review.name = token3.username;
                 review.quote = req.body.quote;
